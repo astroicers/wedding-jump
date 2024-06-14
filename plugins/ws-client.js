48d546222260bf76733b44ece4b892ef7fb42495
Astro Chen <astroicers@snoweasl.onmicrosoft.com>
2024-06-14 13:15:02 +0800

    chore: Update WebSocket URL to use secure connection

--- plugins/ws-client.js
+++ plugins/ws-client.js
@@ -1,6 +1,6 @@
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.client) {
-    const ws = new WebSocket('ws://wedding-jump-production.up.railway.app:3001');
+    const ws = new WebSocket('wss://wedding-jump-production.up.railway.app:3001');
     
     ws.onopen = () => {
       console.log('WebSocket connected.');
